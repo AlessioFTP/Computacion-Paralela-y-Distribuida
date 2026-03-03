20941387e3b3f108f68c0ecb075009b6d5087ec1
AlessioFTP <jrojas78@uan.edu.co>
2026-03-03 11:40:08 -0500

    Corrección de la medición del tiempo en paralelo. Agregación y comparación del proceso pero de manera secuencial.

--- Taller1/src/main/java/org/example/Main.java
+++ Taller1/src/main/java/org/example/Main.java
@@ -1,10 +1,11 @@
 package org.example;
 
 import org.example.logica.HiloLista;
+import org.example.logica.ListaSecuencial;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
 
 public class Main {
     public static void main(String[] args) {
@@ -21,19 +22,43 @@ public class Main {
 
         hilos.forEach(Thread::start);
 
-        int sumaTotal = 0;
-        long tiempoTotal = 0;
+        int sumaTotalParalelo = 0;
+        long tiempoInicioParalelo = System.nanoTime();
+
         for (HiloLista hilo : hilos) {
             try {
                 hilo.join();
-                sumaTotal += hilo.getSumaNumerosLista();
-                tiempoTotal += hilo.getTiempoEjecucion();
+                sumaTotalParalelo += hilo.getSumaNumerosLista();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        sumaTotalParalelo += 5000;
 
-        System.out.println("Suma total: " + sumaTotal);
-        System.out.println("Tiempo total: " + tiempoTotal + " nanosegundos");
+        long tiempoFinParalelo = System.nanoTime();
+        long tiempoTotalParalelo = tiempoFinParalelo - tiempoInicioParalelo;
+
+        System.out.println("Suma total: " + sumaTotalParalelo);
+        System.out.println("Tiempo total: " + tiempoTotalParalelo + " nanosegundos");
+
+
+        System.out.println();
+        List<ListaSecuencial> secuenciales = new ArrayList<>();
+        secuenciales.add(new ListaSecuencial(5000, 0, 1, new ArrayList<>(), listaNumeros));
+        secuenciales.add(new ListaSecuencial(5000, 0, 0, new ArrayList<>(), listaNumeros));
+        secuenciales.add(new ListaSecuencial(10000, 5000, 1, new ArrayList<>(), listaNumeros));
+        secuenciales.add(new ListaSecuencial(10000, 5000, 0, new ArrayList<>(), listaNumeros));
+
+        long tiempoTotalSecuencial = 0;
+        int sumaTotalSecuencial = 0;
+        for (ListaSecuencial sec : secuenciales) {
+            sec.empezarMapeoSublistas();
+            tiempoTotalSecuencial += sec.getTiempoEjecucion();
+            sumaTotalSecuencial += sec.getSumaNumerosLista();
+        }
+
+        System.out.println("Suma total en secuencial: " + sumaTotalSecuencial);
+        System.out.println("Tiempo total en secuencial: " + tiempoTotalSecuencial + " nanosegundos");
+
     }
 }
--- Taller1/src/main/java/org/example/logica/ListaSecuencial.java
+++ Taller1/src/main/java/org/example/logica/ListaSecuencial.java
@@ -2,15 +2,16 @@ package org.example.logica;
 
 import java.util.ArrayList;
 
-public class HiloLista extends Thread{
+public class ListaSecuencial {
     private final int limiteSuperior;
     private final int limiteInferior;
     private final int resultadoModulo; // 0 si es par, 1 si es impar
     private int sumaNumerosLista = 0;
+    private long tiempoEjecucion;
     private final ArrayList<Integer> sublista;
     private final ArrayList<Integer> lista;
 
-    public HiloLista (int limiteSuperior, int limiteInferior, int resultadoModulo,ArrayList<Integer> sublista, ArrayList<Integer> lista){
+    public ListaSecuencial (int limiteSuperior, int limiteInferior, int resultadoModulo,ArrayList<Integer> sublista, ArrayList<Integer> lista){
         this.limiteSuperior = limiteSuperior;
         this.limiteInferior = limiteInferior;
         this.resultadoModulo = resultadoModulo;
@@ -18,14 +19,13 @@ public class HiloLista extends Thread{
         this.lista = lista;
     }
 
-    public int getSumaNumerosLista(){
-        return sumaNumerosLista;
-    }
+    public int getSumaNumerosLista(){ return sumaNumerosLista;}
+
+    public long getTiempoEjecucion(){ return tiempoEjecucion;}
 
     //public ArrayList<Integer> getSublista(){ return sublista;} Des comentar si se requiere saber cúal fue la sublista que se creó
 
-    @Override
-    public void run(){
+    public void empezarMapeoSublistas(){
         long inicioTiempo = System.nanoTime();
         for (Integer numero : lista) {
             if (numero >= limiteInferior && numero < limiteSuperior && numero % 2 == resultadoModulo) {
@@ -34,7 +34,7 @@ public class HiloLista extends Thread{
             }
         }
         long finalTiempo = System.nanoTime();
-        long tiempoEjecucion = finalTiempo - inicioTiempo;
+        tiempoEjecucion = finalTiempo - inicioTiempo;
         String[] tipoDeLista = {"pares", "impares"};
         System.out.println("La lista de números " + tipoDeLista[resultadoModulo] + " con números entre " + limiteInferior + " y " + (limiteSuperior - 1) + ", duró " + tiempoEjecucion + " nanosegundos.");
     }
